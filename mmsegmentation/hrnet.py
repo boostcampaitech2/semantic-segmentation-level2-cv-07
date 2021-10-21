762a73c5f06a10894694ebf2d8e974bfe2baaaad
ppjh8263 <ppjh8263@naver.com>
2021-10-21 13:47:36 +0000

    upload : uplaod all code git practice

--- mmsegmentation/hrnet.py
+++ mmsegmentation/hrnet.py
@@ -4,53 +4,56 @@
 norm_cfg = dict(type='BN', requires_grad=True)
 model = dict(
     type='EncoderDecoder',
-    pretrained=None,
+    pretrained='open-mmlab://msra/hrnetv2_w48',
     backbone=dict(
-        type='UNet',
-        in_channels=3,
-        base_channels=64,
-        num_stages=5,
-        strides=(1, 1, 1, 1, 1),
-        enc_num_convs=(2, 2, 2, 2, 2),
-        dec_num_convs=(2, 2, 2, 2),
-        downsamples=(True, True, True, True),
-        enc_dilations=(1, 1, 1, 1, 1),
-        dec_dilations=(1, 1, 1, 1),
-        with_cp=False,
-        conv_cfg=None,
+        type='HRNet',
         norm_cfg=norm_cfg,
-        act_cfg=dict(type='ReLU'),
-        upsample_cfg=dict(type='InterpConv'),
-        norm_eval=False),
+        norm_eval=False,
+        extra=dict(
+            stage1=dict(
+                num_modules=1,
+                num_branches=1,
+                block='BOTTLENECK',
+                num_blocks=(4, ),
+                num_channels=(64, )),
+            stage2=dict(
+                num_modules=1,
+                num_branches=2,
+                block='BASIC',
+                num_blocks=(4, 4),
+                num_channels=(48, 96)),
+            stage3=dict(
+                num_modules=4,
+                num_branches=3,
+                block='BASIC',
+                num_blocks=(4, 4, 4),
+                num_channels=(48, 96, 192)),
+            stage4=dict(
+                num_modules=3,
+                num_branches=4,
+                block='BASIC',
+                num_blocks=(4, 4, 4, 4),
+                num_channels=(48, 96, 192, 384)))),
     decode_head=dict(
-        type='ASPPHead',
-        in_channels=64,
-        in_index=4,
-        channels=16,
-        dilations=(1, 12, 24, 36),
-        dropout_ratio=0.1,
+        type='FCNHead',
+        in_channels=[48, 96, 192, 384],
+        in_index=(0, 1, 2, 3),
+        channels=sum([48, 96, 192, 384]),
+        input_transform='resize_concat',
+        kernel_size=1,
+        num_convs=1,
+        concat_input=False,
+        dropout_ratio=-1,
         num_classes=11,
         norm_cfg=norm_cfg,
         align_corners=False,
         loss_decode=dict(
             type='CrossEntropyLoss', use_sigmoid=False, loss_weight=1.0)),
-    auxiliary_head=dict(
-        type='FCNHead',
-        in_channels=128,
-        in_index=3,
-        channels=64,
-        num_convs=1,
-        concat_input=False,
-        dropout_ratio=0.1,
-        num_classes=11,
-        norm_cfg=norm_cfg,
-        align_corners=False,
-        loss_decode=dict(
-            type='CrossEntropyLoss', use_sigmoid=False, loss_weight=0.4)),
     # model training and testing settings
     train_cfg=dict(),
     test_cfg=dict(mode='slide', crop_size=(256,256), stride=(170,170)))
     # test_cfg=dict(mode='whole')))
+
 
 
 ##########################################################
--- mmsegmentation/train.py
+++ mmsegmentation/train.py
@@ -4,24 +4,16 @@ from mmseg.apis import train_segmentor
 from mmcv import Config
 
 # cfg = Config.fromfile('/opt/ml/semantic-segmentation-level2-cv-07/mmsegmentation/deeplabv4_unet_s5-d16.py')
-cfg = Config.fromfile('deeplabv4_unet_s5-d16.py')
-# Build the dataset
-datasets = [build_dataset(cfg.data.train)]
+# cfg = Config.fromfile('deeplabv4_unet_s5-d16.py')
+cfg = Config.fromfile('hrnet.py')
 
-# print('='*30)
-# print(datasets)
-# print('='*30)
+datasets = [build_dataset(cfg.data.train)]
 
 cfg.seed=2021
 cfg.gpu_ids = [0]
 
 
-# cfg.work_dir = './work_dirs/swin_decetors'
-cfg.work_dir = './work_dirs/test'
-# Build the detector
-# model = build_segmentor(
-    # cfg.model, train_cfg=cfg.get('train_cfg'), test_cfg=cfg.get('test_cfg'))
-# model = build_segmentor(cfg.model, train_cfg=cfg.get('train_cfg'))
+cfg.work_dir = './work_dirs/hrnet'
 model = build_segmentor(cfg.model)
 
 # print(datasets[0])
