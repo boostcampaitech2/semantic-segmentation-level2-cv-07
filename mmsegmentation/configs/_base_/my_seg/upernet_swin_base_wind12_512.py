4d14fb8d3179cd062a4966554fe11b8ee902e7fc
ksm <7tkfkd@naver.com>
2021-10-29 14:25:58 +0000

    add crf_viz and fix swin_base a little bit

--- mmsegmentation/configs/_base_/my_seg/upernet_swin_base_wind12_512.py
+++ mmsegmentation/configs/_base_/my_seg/upernet_swin_base_wind12_512.py
@@ -2,6 +2,7 @@ _base_ = [
     'upernet_swin_tiny_patch4_window7_512x512_160k_ade20k_'
     'pretrain_224x224_1K.py'
 ]
+
 model = dict(
     pretrained='https://download.openmmlab.com/mmsegmentation/v0.5/swin/upernet_swin_base_patch4_window12_512x512_160k_ade20k_pretrain_384x384_22K/upernet_swin_base_patch4_window12_512x512_160k_ade20k_pretrain_384x384_22K_20210531_125459-429057bf.pth',
     backbone=dict(
@@ -11,4 +12,6 @@ model = dict(
         num_heads=[4, 8, 16, 32],
         window_size=12),
     decode_head=dict(in_channels=[128, 256, 512, 1024], num_classes=11),
-    auxiliary_head=dict(in_channels=512, num_classes=11))
+    auxiliary_head=dict(in_channels=512, num_classes=11),
+    # init_cfg=dict(type='Pretrained', checkpoint=pretrained),# pretrained=pretrained
+    )
