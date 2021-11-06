4cb908b3c461a71f909a545570e8c7768e5b3312
ksm <7tkfkd@naver.com>
2021-11-06 23:53:22 +0900

    cleaning_main_page

--- mmsegmentation/inference.py
+++ mmsegmentation/inference.py
@@ -1,3 +1,9 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
 import os
 import mmcv
 
@@ -13,41 +19,65 @@ import numpy as np
 import json
 
 
-cfg = Config.fromfile('deeplabv4_unet_s5-d16.py')
+# In[2]:
 
-epoch = 'latest'
-cfg.seed=2021
-cfg.gpu_ids = [0]
 
-cfg.work_dir = './work_dirs/test'
+# config file 들고오기
+cfg = Config.fromfile('./configs/_base_/my_seg/upernet_swin_base_wind12_512.py')
 
+epoch = 'epoch_29'
+
+# cfg.model.decode_head.num_classes=11
+# cfg.model.auxiliary_head.num_classes=11
+
+# dataset config 수정
+cfg.data.test.test_mode = True
+
+cfg.data.samples_per_gpu = 8
+
+cfg.work_dir = './work_dirs/up_swi_psuedo_variRGBblurRotateTra'
+
+cfg.optimizer_config.grad_clip = dict(max_norm=35, norm_type=2)
+cfg.model.train_cfg = None
+
+# checkpoint path
 checkpoint_path = os.path.join(cfg.work_dir, f'{epoch}.pth')
 
-dataset = build_dataset(cfg.data.test)
 
+# In[3]:
+
+
+dataset = build_dataset(cfg.data.test)
 data_loader = build_dataloader(
         dataset,
-        samples_per_gpu=1,
+        samples_per_gpu=cfg.data.samples_per_gpu,
         workers_per_gpu=cfg.data.workers_per_gpu,
         dist=False,
         shuffle=False)
 
-model = build_segmentor(cfg.model)
 
-model.CLASSES = ("Backgroud", "General trash", "Paper", "Paper pack",
-                    "Metal", "Glass", "Plastic", "Styrofoam",
-                    "Plastic bag", "Battery", "Clothing")
+# In[4]:
+
 
 model = build_segmentor(cfg.model, test_cfg=cfg.get('test_cfg'))
 checkpoint = load_checkpoint(model, checkpoint_path, map_location='cpu')
 
+model.CLASSES = dataset.CLASSES
 model = MMDataParallel(model.cuda(), device_ids=[0])
+
+
+# In[5]:
+
 
 output = single_gpu_test(model, data_loader)
 
+
+# In[6]:
+
+
 # sample_submisson.csv 열기
 submission = pd.read_csv('./work_dirs/sample_submission.csv', index_col=None)
-json_dir = os.path.join("../input/data/test.json")
+json_dir = os.path.join("/opt/ml/segmentation/input/data/test.json")
 with open(json_dir, "r", encoding="utf8") as outfile:
     datas = json.load(outfile)
 
@@ -74,3 +104,27 @@ for image_id, predict in enumerate(output):
 
 # submission.csv로 저장
 submission.to_csv(os.path.join(cfg.work_dir, f'submission_{epoch}.csv'), index=False)
+
+
+# In[7]:
+
+
+# # reference : https://stackoverflow.com/questions/48121916/numpy-resize-rescale-image
+# input_size = 512
+# output_size = 256
+# bin_size = input_size // output_size
+
+# for image_id, predict in enumerate(output):
+#   temp_mask = []
+#   predict = predict.reshape(1, 512, 512)
+#   print(predict.shape)
+#   mask = predict.reshape((1, output_size, bin_size, output_size, bin_size)).max(4).max(2)
+#   print(mask.shape)
+#   temp_mask.append(mask)
+  
+#   oms = np.array(temp_mask)
+#   oms = oms.reshape([oms.shape[0], output_size*output_size]).astype(int)
+#   print(oms.shape)
+#   predict = oms
+#   print(predict.shape)
+
--- mmsegmentation/train.py
+++ mmsegmentation/train.py
@@ -1,37 +1,76 @@
+# !apt-get update
+# !apt-get install -y libsm6 libxext6 libxrender-dev
+# !pip install opencv-python
+
+#!pip install openmim
+
+# 모듈 import
+
+from mmcv import Config
+from mmseg.apis import train_segmentor
 from mmseg.datasets import build_dataset
 from mmseg.models import build_segmentor
-from mmseg.apis import train_segmentor
-from mmcv import Config
 
-# cfg = Config.fromfile('/opt/ml/semantic-segmentation-level2-cv-07/mmsegmentation/deeplabv4_unet_s5-d16.py')
-cfg = Config.fromfile('deeplabv4_unet_s5-d16.py')
-# Build the dataset
-datasets = [build_dataset(cfg.data.train)]
+# config file 들고오기
+cfg = Config.fromfile('./configs/_base_/my_seg/upernet_swin_base_wind12_512.py')
 
-# print('='*30)
-# print(datasets)
-# print('='*30)
+cfg.data.samples_per_gpu = 8
+cfg.data.workers_per_gpu = 4
 
-cfg.seed=2021
+cfg.seed = 2021
 cfg.gpu_ids = [0]
 
+cfg.work_dir = './work_dirs/our_final_obj_reduce'
 
-# cfg.work_dir = './work_dirs/swin_decetors'
-cfg.work_dir = './work_dirs/test'
-# Build the detector
-# model = build_segmentor(
-    # cfg.model, train_cfg=cfg.get('train_cfg'), test_cfg=cfg.get('test_cfg'))
-# model = build_segmentor(cfg.model, train_cfg=cfg.get('train_cfg'))
+# cfg.model.decode_head.num_classes=11
+# cfg.model.auxiliary_head.num_classes=11
+
+# image_size 변환 - default : (1024, 1024), 적용시 (2048, 2048)
+# cfg.train_pipeline[2].img_scale=(384,384)
+# cfg.test_pipeline[1].img_scale=(2048,2048)
+# cfg.val_pipeline[2].img_scale=(2048,2048)
+
+# cfg.optimizer_config.grad_clip = dict(max_norm=35, norm_type=2)
+cfg.checkpoint_config = dict(max_keep_ckpts=30, interval=1)
+cfg.runner = dict(type='EpochBasedRunner', max_epochs=50)
+cfg.lr_config =  dict(
+    policy='CosineAnnealing',
+    warmup='linear',
+    warmup_iters=300,
+    warmup_ratio=0.001,
+    min_lr_ratio=1e-5,
+)
+
+cfg.optimizer = dict(
+    type='AdamW',
+    lr=0.00006,
+    betas=(0.9, 0.999),
+    weight_decay=0.01,
+    paramwise_cfg=dict(
+        custom_keys={
+            'absolute_pos_embed': dict(decay_mult=0.),
+            'relative_position_bias_table': dict(decay_mult=0.),
+            'norm': dict(decay_mult=0.)
+        }))
+
+# wandb log설정
+cfg.log_config = dict(
+    interval=50,
+    hooks=[
+        dict(type='TextLoggerHook'),
+        dict(type='EpochWandbLogger',
+        init_kwargs=dict(
+            project='Swin_seg_mmseg',
+            name='our_final_obj_reduce'))
+])
+cfg.workflow = [('train', 1),('val',1)]# 
+cfg.evaluation = dict(interval=1, by_epoch=True, metric='mIoU')
+
+
+# build_dataset
+datasets = [build_dataset(cfg.data.train), build_dataset(cfg.data.val_loss)] #
+
 model = build_segmentor(cfg.model)
+model.init_weights()
 
-# print(datasets[0])
-
-
-# Add an attribute for visualization convenience
-# model.CLASSES = datasets[0].CLASSES
-model.CLASSES = ("Backgroud", "General trash", "Paper", "Paper pack",
-                    "Metal", "Glass", "Plastic", "Styrofoam",
-                    "Plastic bag", "Battery", "Clothing")
-# Create work_dir
-train_segmentor(model, datasets, cfg, distributed=False, validate=False, 
-                meta=dict())
+train_segmentor(model, datasets, cfg, distributed=False, validate=True)
